521561ffb4dcb2fa4942ebfd9cb1020408d5a1b5
IgorPotapoff <gloryhunter82@gmail.com>
2023-07-29 18:24:46 +0300

    table

---
+++ GB/Bootcamp/Lesson3/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+Console.Clear();
+Console.WriteLine("Input a nunber of multiplication table");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+Console.WriteLine();
+
+
+for (int i = 1; i < n + 1; i++)
+{
+
+    for (int j = 1; j < n + 1; j++)
+    {
+
+        Console.Write(i * j);
+        Console.Write("\t");
+
+    }
+
+    Console.WriteLine();
+
+
+}
