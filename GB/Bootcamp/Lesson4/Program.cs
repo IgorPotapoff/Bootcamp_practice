103aa828775311cdc21b97ed186c235aadd39a04
IgorPotapoff <gloryhunter82@gmail.com>
2023-07-30 12:03:01 +0300

    lesson4

---
+++ GB/Bootcamp/Lesson4/Program.cs
@@ -0,0 +1,34 @@
+﻿void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-10, 11); // [-10; 10]
+}
+
+
+int[] SortVibor(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        int min = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[min])
+            {
+                min = j;
+            }
+        }
+        int temp = array[min];
+        array[min] = array[i];
+        array[i] = temp;
+    }
+    return array;
+}
+
+
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = int.Parse(Console.ReadLine()!);
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"Исходный массив: [{string.Join(", ", array)}]");
+Console.WriteLine($"Конечныый массив: [{string.Join(", ", SortVibor(array))}]");
