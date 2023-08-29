dd0867a47ee7e02bdb54a75750681cbc6118d5fe
IgorPotapoff <135151341+IgorPotapoff@users.noreply.github.com>
2023-08-29 15:28:55 +0300

    Update and rename task.py to task20.py

---
+++ GB/Знакомство с Python/Homeworks/1/task20.py
@@ -0,0 +1,39 @@
+points_en = {1: 'AEIOULNSTR', 2: 'DG', 3: 'BCMP', 4: 'FHVWY', 5: 'K', 8: 'JX', 10: 'QZ'}
+points_ru = {1: 'АВЕИНОРСТ', 2: 'ДКЛМПУ', 3: 'БГЁЬЯ', 4: 'ЙЫ', 5: 'ЖЗХЦЧ', 8: 'ШЭЮ', 10: 'ФЩЪ'}
+word = k.upper()  # переводим все буквы в верхний регистр
+count = 0
+for i in word:
+    if i in 'QWERTYUIOPASDFGHJKLZXCVBNM':
+        for j in points_en:
+            if i in points_en[j]:
+                count = count + j
+    else:
+        for j in points_en:
+            if i in points_ru[j]:
+                count = count + j
+print(count)
+
+
+# # import re
+# def isCyrillic(text):
+# 	return bool(re.search('[а-яА-Я]', text))
+# points_en = {1:'AEIOULNSTR',
+#       	2:'DG',
+#       	3:'BCMP',
+#       	4:'FHVWY',
+#       	5:'K',
+#       	8:'JZ',
+#       	10:'QZ'}
+k = 'НОУТБУК'
+points_ru = {1:'АВЕИНОРСТ',
+      	2:'ДКЛМПУ',
+      	3:'БГЁЬЯ',
+      	4:'ЙЫ',
+      	5:'ЖЗХЦЧ',
+      	8:'ШЭЮ',
+      	10:'ФЩЪ'}
+
+# if isCyrillic(text):
+print(sum([b for i in k for b, v in points_ru.items() if i in v]))
+# else:
+# 	print(sum([b for i in text for b, v in points_en.items() if i in v]))
